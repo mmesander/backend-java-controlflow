5744f34ad3e7588be24f136f7d1aadc59a317cee
Mark Mesander <mark_mesander@hotmail.com>
2023-08-29 19:36:09 +0200

    complete B

--- src/main/java/B_WhileLoops.java
+++ src/main/java/B_WhileLoops.java
@@ -1,43 +1,44 @@
 public class B_WhileLoops {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    //
-    // WhileLoops 1
-    //
+        //
+        // WhileLoops 1
+        //
 
-    int i = 0;
-    // Voeg een while loop toe die van 1 tot en met 10 telt.
-
-      System.out.println(i);
-
-
-
-    //
-    // WhileLoops 2
-    //
-
-    int x = 20;
-    int y = 3;
-    // Voeg een while loop toe zodat onderstaand statement wordt geprint zolang x of y niet kleiner worden dan 1
-
-      System.out.println("Size = " + x-- + " x " + y--);
+        int i = 0;
+        // Voeg een while loop toe die van 1 tot en met 10 telt.
+        while (i++ < 10) {
+            System.out.println(i);
+        }
 
 
+        //
+        // WhileLoops 2
+        //
 
-    //
-    // WhileLoops 3
-    //
+        int x = 20;
+        int y = 3;
+        // Voeg een while loop toe zodat onderstaand statement wordt geprint zolang x of y niet kleiner worden dan 1
 
-    int smallest = 0;
-    int largest = 10;
-    int numberOfLoops = 0;
-    while(smallest < largest) {
-      // pas de increment en decrement van smallest en largest aan zodat de loop 4x wordt uitgevoerd.
-      smallest += 1;
-      largest -= 0;
-      numberOfLoops++;
+        while (x > 0 && y > 1) {
+            System.out.println("Size = " + x-- + " x " + y--);
+        }
+
+
+        //
+        // WhileLoops 3
+        //
+
+        int smallest = 0;
+        int largest = 10;
+        int numberOfLoops = 0;
+        while (smallest < largest) {
+            // pas de increment en decrement van smallest en largest aan zodat de loop 4x wordt uitgevoerd.
+            smallest += 2;
+            largest -= 1;
+            numberOfLoops++;
+        }
+        System.out.println(numberOfLoops);
     }
-    System.out.println(numberOfLoops);
-  }
 }
